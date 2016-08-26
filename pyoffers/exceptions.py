8ee988a9ce28b53ab27bae6f83c144ca2f63c1fb
Dmitry Dygalo <dadygalo@gmail.com>
2016-08-26 19:00:15 +0200

    Simplify

--- pyoffers/exceptions.py
+++ pyoffers/exceptions.py
@@ -5,14 +5,6 @@ class HasOffersException(BaseException):
     pass
 
 
-class NetworkIDError(HasOffersException):
-    pass
-
-
-class InvalidPathError(HasOffersException):
-    pass
-
-
 class InputError(HasOffersException):
     pass
 
@@ -23,9 +15,4 @@ def check_errors(errors):
     """
     if not errors:
         return
-    if isinstance(errors, dict):
-        if errors['publicMessage'] == 'Network id is not valid':
-            raise NetworkIDError(errors['publicMessage'])
-        if errors['publicMessage'] == 'Invalid path for URL':
-            raise InvalidPathError(errors['publicMessage'])
     raise InputError(errors)
--- tests/test_api.py
+++ tests/test_api.py
@@ -1,14 +1,14 @@
 # coding: utf-8
 import pytest
 
-from pyoffers.exceptions import NetworkIDError
+from pyoffers.exceptions import InputError
 
 
 def test_invalid_network_id(api):
     old_token = api.network_token
     try:
         api.network_token = 'invalid'
-        with pytest.raises(NetworkIDError):
+        with pytest.raises(InputError):
             api._call('test', 'test')
     finally:
         api.network_token = old_token
